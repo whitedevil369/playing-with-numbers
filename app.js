28a77152e87ad19cd15222552ff3e1040287707d
Poornesh Adhithya <56251750+whitedevil369@users.noreply.github.com>
2020-09-30 15:43:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
-
+const PORT = 3000
+   
+app.listen(PORT, () => {
+    console.log("Server Running on Port: ",PORT);
+    
+    function palindrome(str) {
     /*
     PLAINDROME: This a function which checks whether the given input is same,
     when read from right to left and left to right.
     Eg, IEEE-EEEI
     */
-
-    function palindrome(str) {
-       
     }
 
     function armstrong(n) {
@@ -32,57 +33,4 @@ app.listen(3000, () => {
 
     }
 
-/*
-
-    app.get("/pal", (req, res) => {
-      res.json({
-        result: palindrome(param1),
-        meta: {
-          success: true,
-          code: 200
-        }
-      });
-    });
-
-    app.get("/arm", (req, res) => {
-      res.json({
-        result: armstrong(param2),
-        meta: {
-          success: true,
-          code: 200
-        }
-      });
-    });
-
-    app.get("/prime", (req, res) => {
-      res.json({
-        result: prime(param3),
-        meta: {
-          success: true,
-          code: 200
-        }
-      });
-    });
-
-    app.get("/rev", (req, res) => {
-      res.json({
-         result: reverse(param4),
-        meta: {
-          success: true,
-          code: 200
-        }
-      });
-    });
-
-    app.get("/pattern", (req, res) => {
-      res.json({
-        result: pattern(param5),
-        meta: {
-          success: true,
-          code: 200
-        }
-      });
-    });
-    */
-    console.log("Server running on port 3000");
 });
