a8f27f2be72723c3293a82ebbd4b0090fc62e4b8
Poornesh Adhithya <56251750+whitedevil369@users.noreply.github.com>
2020-09-30 15:37:26 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,6 @@
 const express = require("express");
 var app = express();
 
-var param1 = "1001"
-var param2 = 371
-var param3 = 7
-var param4 = 12345
-var param5 = 4
-
 app.get("/pal", (req, res) => {
   res.json({
     result: palindrome(param1),
@@ -18,42 +12,21 @@ app.get("/pal", (req, res) => {
 });
 
 app.get("/arm", (req, res) => {
-  res.json({
-    result: armstrong(param2),
-    meta: {
-      success: true,
-      code: 200
-    }
-  });
+ //Add Your Code Here
 });
 
 app.get("/prime", (req, res) => {
-  res.json({
-    result: prime(param3),
-    meta: {
-      success: true,
-      code: 200
-    }
-  });
+ //Add Your Code Here
 });
 
 app.get("/rev", (req, res) => {
-  res.json({
-     result: reverse(param4),
-    meta: {
-      success: true,
-      code: 200
-    }
-  });
+ //Add Your Code Here
 });
 
 app.get("/pattern", (req, res) => {
-  res.json({
-    result: pattern(param5),
-    meta: {
-      success: true,
-      code: 200
-    }
-  });
+ //Add Your Code Here
 });
 
+app.get("/fibo", (req, res) => {
+ //Add Your Code Here
+});
