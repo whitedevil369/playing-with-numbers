f1bfd10627afb3b013a876f0c7a41f47dc596834
Poornesh Adhithya <56251750+whitedevil369@users.noreply.github.com>
2020-09-30 16:05:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
 var express = require("express");
 var app = express();
+
+const bodyParser = require("body-parser");
 const PORT = 3000
-   
+
+app.use(bodyParser.json());
+app.use('/routes', routes));
+
 app.listen(PORT, () => {
     console.log("Server Running on Port: ",PORT);
-    
-    function palindrome(str) {
-    /*
-    PLAINDROME: This a function which checks whether the given input is same,
-    when read from right to left and left to right.
-    Eg, IEEE-EEEI
-    */
-    }
-
-    function armstrong(n) {
-       
-    }
-
-    function prime(n) {
-
-    }
-
-    function reverse(n) {
-
-    }
-
-    function pattern(n) {
-
-    }
-
-    function fibonacci(n) {
-
-    }
-
 });
