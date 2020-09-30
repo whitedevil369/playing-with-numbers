c808a3d5866fa501b3c42c7b59744a5728b18e98
Poornesh Adhithya <56251750+whitedevil369@users.noreply.github.com>
2020-09-30 16:51:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var app = express();
-const routes = require("express")
+const routes = require("./routes/route.js")
 const bodyParser = require("body-parser");
 const PORT = 3000
 
 app.use(bodyParser.json());
-app.use('/',routes);
+app.use('/app',routes);
 
 app.listen(PORT, () => {
     console.log("Server Running on Port: ",PORT);
